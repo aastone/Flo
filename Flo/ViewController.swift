c0741eb7124c90b17345b2258ee01bfac8358bef
stone <stoneiswang@gmail.com>
2017-01-13 16:37:06 +0800

    Update UI

--- Flo/ViewController.swift
+++ Flo/ViewController.swift
@@ -10,8 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var containerView: UIView!
+    @IBOutlet weak var graphView: GraphView!
     @IBOutlet weak var counterView: CounterView!
     @IBOutlet weak var counterLabel: UILabel!
+    
+    var isGraphViewShowing = false
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -26,12 +32,34 @@ class ViewController: UIViewController {
     @IBAction func addButtonPressed(_ sender: Any) {
         counterView.counter += 1
         counterLabel.text = String(counterView.counter)
+        if isGraphViewShowing {
+//            counterViewTap(nil)
+        }
     }
 
     @IBAction func decreaseButtonPressed(_ sender: Any) {
         counterView.counter -= 1
         counterLabel.text = String(counterView.counter)
+        if isGraphViewShowing {
+//            counterViewTap(nil)
+        }
     }
 
+    @IBAction func counterViewTap(_ sender: UITapGestureRecognizer) {
+        if isGraphViewShowing {
+//            UIView.transition(from: graphView,
+//                              to: counterView,
+//                              duration: 1.0,
+//                              options: UIViewAnimationOptions.transitionFlipFromLeft | UIViewAnimationOptions.showHideTransitionViews,
+//                              completion: nil)
+        } else {
+//            UIView.transition(from: counterView,
+//                              to: graphView,
+//                              duration: 1.0,
+//                              options: UIViewAnimationOptions.transitionFlipFromRight | UIViewAnimationOptions.showHideTransitionViews,
+//                              completion: nil)
+        }
+        isGraphViewShowing = !isGraphViewShowing
+    }
 }
 
